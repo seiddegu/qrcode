c5a79d5d51395aa474a4234a15c123a61efd6cad
seid degu <seiddegu@gmail.com>
2021-05-06 23:31:26 +0300

    initial commit

---
+++ qrcodeGen.py
@@ -0,0 +1,27 @@
+import qrcode
+
+input_data = "http://www.ethiotelcom.et"
+
+def readData(fname):
+    dataList = []
+    with open(fname, 'r') as fd:
+        for line in fd:
+            dataList.append(line.strip())
+            # print(line, end='')
+    fd.close()
+    return dataList 
+
+# Creating an instance of qrcode
+qr = qrcode.QRCode(
+        version=1,
+        box_size=7,
+        border=3)
+fname = "qrdata.txt"
+getData = readData(fname)
+for pattern in getData:
+    qr.add_data(pattern)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    qrFname = "qrCode" + pattern + ".png"
+    img.save(qrFname)
+# print(readData(fname))
