1c8dc25a660373af9746dc421ef926dea9c8f1e6
seid degu <seiddegu@gmail.com>
2021-05-06 23:36:44 +0300

    data clearing

--- qrcodeGen.py
+++ qrcodeGen.py
@@ -1,3 +1,7 @@
+# install these two packages via pip to make the code work.
+# pip install pillow
+# pip install qrcode
+
 import qrcode
 
 input_data = "http://www.ethiotelcom.et"
@@ -16,12 +20,12 @@ qr = qrcode.QRCode(
         version=1,
         box_size=7,
         border=3)
-fname = "qrdata.txt"
+fname = "inputData.txt"
 getData = readData(fname)
 for pattern in getData:
     qr.add_data(pattern)
     qr.make(fit=True)
     img = qr.make_image(fill='black', back_color='white')
-    qrFname = "qrCode" + pattern + ".png"
+    qrFname = "genratedqrCode/"+"qrCode" + pattern + ".png"
     img.save(qrFname)
 # print(readData(fname))
